3a8af2573fb84e76f4d1ebe8671ce4efb0a88626
Amal Abdu <abduamal@ymail.com>
2021-09-02 13:12:07 -0500

    install Devise, create User model + add fields

---
+++ db/migrate/20210902180803_add_columns_to_user.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :middle_initial, :string
+    add_column :users, :last_name, :string
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+  end
+end
