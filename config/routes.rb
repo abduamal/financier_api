0c38f010cc576bae890cc5cd5dc0b5cfb5b3078a
Amal Abdu <abduamal@ymail.com>
2021-09-03 14:41:22 -0500

    update routes to pull from new controllers in user folder

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
 
   scope :api, defaults: { format: :json } do
 
-     devise_for :users
+    devise_for :users,
+               controllers: {
+                   sessions: 'users/sessions',
+                   registrations: 'users/registrations'
+               }
+    get '/member-data', to: 'members#show'
 
      resources :accounts do
        resources :account_transactions
      end
-     
+
    end
 
 end
