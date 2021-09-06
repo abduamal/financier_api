35a9b41be13ea6ae6ccb54a0d07582961a851df4
Amal Abdu <abduamal@ymail.com>
2021-09-06 09:21:03 -0500

    add validations

--- app/models/account_transaction.rb
+++ app/models/account_transaction.rb
@@ -1,4 +1,7 @@
 class AccountTransaction < ApplicationRecord
   belongs_to :account
   has_many :account_transactions
+
+  validates :amount, presence: true
+  validates_inclusion_of :transaction_type, :in => ['deposit', 'withdraw']
 end
